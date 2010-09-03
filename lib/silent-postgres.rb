c2c10eeca268151a6db5fe656520365a6d6a6f13
Evgeniy Dolzhenko <dolzenko@gmail.com>
2010-09-03 04:40:07 -0700

    Make autoloadable by Rails 3

--- lib/silent-postgres.rb
+++ lib/silent-postgres.rb
@@ -17,3 +17,9 @@ module SilentPostgres
 METHOD
   end
 end
+
+if %w(test development).include?(ENV["RAILS_ENV"])
+  puts "Silencing Postgres"
+  require 'silent_postgres'
+  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.send(:include, SilentPostgres)
+end
