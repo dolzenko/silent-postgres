33a15f0d5e21b02f97cb45bd09d202aa0549578b
Evgeniy Dolzhenko <dolzenko@gmail.com>
2010-09-03 04:44:09 -0700

    Minor

--- init.rb
+++
@@ -1,5 +0,0 @@
-if %w(test development).include?(ENV["RAILS_ENV"])
-  puts "Silencing Postgres"
-  require 'silent_postgres'
-  ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.send(:include, SilentPostgres)
-end
