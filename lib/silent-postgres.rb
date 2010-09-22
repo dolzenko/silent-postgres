e0015391fb45f7c8929d478f3fdb148e8e537c76
jgn <john@7fff.com>
2010-09-22 14:05:32 -0500

    Tweaks to make it actually work

--- lib/silent-postgres.rb
+++ lib/silent-postgres.rb
@@ -20,6 +20,5 @@ end
 
 if %w(test development).include?(ENV["RAILS_ENV"])
   puts "Silencing Postgres"
-  require 'silent_postgres'
   ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.send(:include, SilentPostgres)
 end
